d374eafaafb0956497af4a0fc10aa24d8ad59ee7
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2023-09-28 11:32:55 +0200

    Creates a model class for city entity

---
+++ SolarWatch/Model/City.cs
@@ -0,0 +1,10 @@
+﻿namespace SolarWatch.Model;
+
+public class City
+{
+    public string Name { get; init; }
+    public double Latitude { get; init; }
+    public double Longitude { get; init; }
+    public string State { get; init; }
+    public string Country { get; init; }
+}
