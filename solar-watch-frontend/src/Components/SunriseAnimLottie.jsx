cb224d1c108c36eba1db7640bd030c23e9a4d2ee
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2023-10-13 12:15:30 +0200

    fixes SunriseAnimLottie.jsx to loop properly

--- solar-watch-frontend/src/Components/SunriseAnimLottie.jsx
+++ solar-watch-frontend/src/Components/SunriseAnimLottie.jsx
@@ -1,76 +1,88 @@
 import React, { useEffect, useRef, useState } from 'react';
-import Lottie, { LottieRefCurrentProps } from 'lottie-react';
-// import LottieView from 'lottie-react-native';
+import Lottie from 'lottie-react';
 import sunriseAnim from "../Animations/sunriseAnim.json";
 
 const sunriseFrame = 100;
-const middayFrame = 142;
+const middayFrame = 162;
 const nightFrame = 364;
+const sunsetFrame = 50;
 
-
-const SunriseAnimLottie = () => {
-    
+const SunriseAnimLottie = ({newStopFrame, setNewStopFrame}) => {
     const [stopFrame, setStopFrame] = useState(middayFrame);
-    const [goingUp, setGoingUp] = useState(true);
+    // const [newStopFrame, setNewStopFrame] = useState(middayFrame);
     const [dayNightCycle, setDayNightCycle] = useState(false);
     const lottieRef = useRef(null);
 
-   const SetSunset = () =>{
-        setDayNightCycle(false);
-        setGoingUp(false);
-        lottieRef.current.setDirection(-1);
-        setStopFrame(0);
-        lottieRef.current.play();
-   }
-   const SetSunrise = () =>{
-        var current = lottieRef.current.animationItem.currentFrame;
-        setGoingUp(true);
-        lottieRef.current.setDirection(1);
-        if (current>80) {
-            setDayNightCycle(true);
-            setStopFrame(nightFrame);
-            lottieRef.current.goToAndPlay(current, true);
-        }
-        else{
-            setStopFrame(sunriseFrame);
-            lottieRef.current.goToAndPlay(0, true);
-        }
-        setStopFrame(sunriseFrame)
-    }
-    const SetNight = () =>{
-        setDayNightCycle(false);
-        setGoingUp(true);
-        lottieRef.current.setDirection(1);
-        setStopFrame(nightFrame);
-        lottieRef.current.goToAndPlay(lottieRef.current.animationItem.currentFrame, true); 
-    }
-   
-    useEffect(() => {
-        if (lottieRef.current) {
+    const SetSunset = () => {
+        setNewStopFrame(sunsetFrame);
+    };
 
-            // console.log(lottieRef.current);
+    const SetSunrise = () => {
+        setNewStopFrame(sunriseFrame);
+    };
+
+    const SetNight = () => {
+        setNewStopFrame(nightFrame);
+    };
+
+    const SetMidday = () => {
+        setNewStopFrame(middayFrame);
+    };
+
+    useEffect(() => {
+        //console.log(dayNightCycle);
+        if (lottieRef.current) {
             const animationInterval = setInterval(() => {
                 const currentFrame = lottieRef.current.animationItem.currentFrame;
-                if (goingUp) {
-                    if ((!dayNightCycle) && Math.abs(currentFrame - stopFrame) <= 1.5) {
-                        lottieRef.current.pause();
-                        clearInterval(animationInterval);
-                    } else if (dayNightCycle && Math.abs(currentFrame - stopFrame) <= 3.5) {
-                        setStopFrame(sunriseFrame);
-                        lottieRef.current.goToAndPlay(0, true);
-                        setDayNightCycle(false);
-                        clearInterval(animationInterval);
-                    }
-                } else if (!goingUp && !dayNightCycle && Math.abs(stopFrame - currentFrame) <= 1.5) {
-                    lottieRef.current.pause();
+                var test = false; //This prevents the second else if from running while UseState updates daynightcycle
+                console.log(dayNightCycle);
+                if (currentFrame >=100 && newStopFrame === sunriseFrame) {
+                    console.log(`currFrame: ${currentFrame}, stopFrame: ${newStopFrame}`);
+                    console.log("You want to circle around");
+                    setDayNightCycle(true);
+                    test = true; //This prevents the second else if from running while UseState updates daynightcycle
+                    setStopFrame(nightFrame);
+                }
+                else {
+                    console.log("You don't want to circle around");
+                    setStopFrame(newStopFrame);
+                    setDayNightCycle(false);
+                }
+                //console.log(`currFrame: ${currentFrame}, stopFrame: ${stopFrame}`);
+                if (dayNightCycle && Math.abs(currentFrame - stopFrame) <= 3.5) {
+                    console.log("should cycle around");
+                    setStopFrame(sunriseFrame);
+                    lottieRef.current.goToAndPlay(0, true);
+                    setDayNightCycle(false);
                     clearInterval(animationInterval);
                 }
+                else if (!dayNightCycle && Math.abs(currentFrame - stopFrame) <= 2.5 && !test){
+                    console.log("reached destination");
+                    lottieRef.current.pause();
+                    clearInterval(animationInterval);
+                    setDayNightCycle(false);
+                }
+                
             }, 100);
-            // console.log(lottieRef.current.getDuration());
-        return () => clearInterval(animationInterval);
-
+            return () => clearInterval(animationInterval);
         }
-    }, [lottieRef, stopFrame, dayNightCycle]);
+    }, [lottieRef, stopFrame, dayNightCycle, newStopFrame]);
+
+    useEffect(() => {
+        if (lottieRef.current && lottieRef.current.animationItem) {
+            const currentFrame = lottieRef.current.animationItem.currentFrame;
+
+            if (currentFrame < stopFrame) {
+                // console.log("going up ran");
+                lottieRef.current.setDirection(1); // Play forward
+                lottieRef.current.goToAndPlay(currentFrame, true);
+            } else if (currentFrame > stopFrame) {
+                // console.log("going down ran");
+                lottieRef.current.setDirection(-1); // Play backward
+                lottieRef.current.goToAndPlay(currentFrame, true);
+            }
+        }
+    }, [lottieRef, stopFrame]);
 
     return (
         <div>
@@ -82,6 +94,7 @@ const SunriseAnimLottie = () => {
             <button onClick={SetSunset}>Set sunset</button>
             <button onClick={SetSunrise}>Set sunrise</button>
             <button onClick={SetNight}>Set night</button>
+            <button onClick={SetMidday}>Set midday</button>
         </div>
     );
 };
